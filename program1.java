f24cc7bd73b5021da75462f71fdef90c8baceaa4
saiteja1111n <saiteja1111n@gmal.com>
2015-06-29 17:43:17 +0530

    reduced the charAt calling by storing in integer array

--- program1.java
+++ program1.java
@@ -13,11 +13,13 @@ public class program1 {
 	}
 
 	public static boolean check(String s) {
-		for(int i = 0; i < s.length()-1; i++) {
-			for(int j = i+1; j < s.length(); j++) {
-				int p = s.charAt(i);
-				int q = s.charAt(j);
-				if(p == q) 
+		int[] a = new int[s.length()];
+		for(int i = 0; i < s.length(); i++) {
+			a[i]=s.charAt(i);
+		}
+		for(int i = 0; i < a.length-1; i++) {
+			for(int j = i+1; j < a.length; j++) {
+				if(a[i] == a[j]) 
 					return false;
 			}
 		}
